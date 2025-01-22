a660e051272168743cf99cb1b35d798408a0e06b
Branislav Kadlec <75735222+CrumblyBread@users.noreply.github.com>
2025-01-23 00:19:31 +0100

    Basic Project with a Player Controller

---
+++ crumblyfishing/scripts/player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody3D
+
+@onready var camera = $Camera3D
+@onready var anim = $Camera3D/Hand/AnimationPlayer
+
+const SPEED = 7.0
+const JUMP_VELOCITY = 4.5
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * 0.005)
+		camera.rotate_x(-event.relative.y * 0.005)
+		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
+		
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "up", "down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	if input_dir != Vector2.ZERO and is_on_floor():
+		anim.play("Move")
+	else:
+		anim.play("Idle")
+
+	move_and_slide()
